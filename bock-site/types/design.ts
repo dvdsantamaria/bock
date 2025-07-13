71d763d18df5a5dd6e85b8bb0e8c4ed0d7cddd56
smarttours <devede@me.com>
2025-07-13 15:01:18 +1000

    Add type definitions for about, design and photography

---
+++ bock-site/types/design.ts
@@ -0,0 +1,6 @@
+export interface DesignBlock {
+  id: number;
+  title: string;
+  body: string;
+  slug: string;
+}
---
+++ bock-site/types/photography.ts
@@ -0,0 +1,9 @@
+export interface PhotoItem {
+  id: number;
+  title: string;
+  subtitle?: string;
+  body: any;
+  slug: string;
+  imageThumb?: string;
+  imageFull?: string;
+}
