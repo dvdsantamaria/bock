1c0bc2343dc4f72df9243494abb84537c5255497
smarttours <devede@me.com>
2025-07-15 16:03:27 +1000

    Fix cloudinary no watermark

--- clean-strapi/config/plugins.ts
+++ clean-strapi/config/plugins.ts
@@ -1,3 +1,24 @@
-export default () => ({
-  // Sin configuración extra
-});
+export default ({ env }) => {
+  console.log("🧪 Cloudinary ENV", {
+    name: env("CLOUDINARY_NAME"),
+    key: env("CLOUDINARY_KEY"),
+    secret: env("CLOUDINARY_SECRET"),
+  });
+
+  return {
+    upload: {
+      config: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env("CLOUDINARY_NAME"),
+          api_key: env("CLOUDINARY_KEY"),
+          api_secret: env("CLOUDINARY_SECRET"),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+  };
+};
